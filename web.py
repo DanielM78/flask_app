73601ffe6ce4c7ed075ba49d74ea0f1e0a160929
generic <generic@localhost.localdomain>
2017-03-05 12:07:18 -0500

    trying to fix this

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request 
-<<<<<<< HEAD
+ 
 import weather 
 # import yelp_page 
 app = Flask(__name__)
@@ -19,29 +19,7 @@ def index():
  
 
 if __name__ == "__main__":
-
-    app.run()
-=======
-import weather
-
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    address = request.values.get('address')
-    forecast = None
-    if address:
-        forecast = weather.get_weather(address)
-
-    return render_template('index.html', forecast=forecast)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0",port=port)
 
->>>>>>> 7da1378c68252e91871dbd6f3c82217ceb4a8953
+
